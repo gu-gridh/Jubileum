7f346e808808cc92579d43d65018b5a84e921bbc
AramKarimi <aram.karimi@gu.se>
2023-04-11 17:50:29 +0200

    update admin page

--- admin.py
+++ admin.py
@@ -1,9 +1,54 @@
-from django.contrib import admin
 from django.contrib.gis.db import models
 from .models import *
 from django.utils.html import format_html
 from django.contrib.gis import admin
 from django.utils.translation import gettext_lazy as _
 from diana.utils import get_fields, DEFAULT_FIELDS, DEFAULT_EXCLUDE
+from admin_auto_filters.filters import AutocompleteFilter
+from rangefilter.filters import NumericRangeFilter
+from django.contrib.admin import EmptyFieldListFilter
 from django.conf import settings
+from PIL import Image as ima
+import os
+import base64 
+from io import StringIO
 
+
+@admin.register(Place)
+class PlaceAdmin(admin.GISModelAdmin):
+    list_display = ['name', 'geometry', 'parent_id']
+    search_fields = ['name']
+
+class PlaceFilter(AutocompleteFilter):
+    title = _('Place') # display title
+    field_name = 'place' # name of the foreign key field
+
+@admin.register(Image)
+class ImageModel(admin.ModelAdmin):
+
+    fields              = ['image_preview', *get_fields(Image, exclude=['id'])]
+    readonly_fields     = ['iiif_file', 'uuid', 'image_preview', *DEFAULT_FIELDS]
+    autocomplete_fields = ['place']
+    list_display        = ['thumbnail_preview', 'title']
+    search_fields       = ['place__title']
+    list_filter         = [PlaceFilter]
+    
+    list_per_page = 10
+
+    def image_preview(self, obj):
+        return format_html(f'<img src="{settings.IIIF_URL}{obj.iiif_file}/full/full/0/default.jpg" height="300" />')
+
+    def thumbnail_preview(self, obj):
+        return format_html(f'<img src="{settings.IIIF_URL}{obj.iiif_file}/full/full/0/default.jpg" height="100" />')
+
+
+@admin.register(Layer)
+class LayerAdmin(admin.ModelAdmin):
+    list_display = [*get_fields(Layer, exclude=['id'])]
+    search_fields = ['title']
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = [*get_fields(Source, exclude=['id'])]
+    search_fields = ['title']
