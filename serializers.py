a4278ada11c124b13b63556c08cc4eeb6fad745e
AramKarimi <aram.karimi@gu.se>
2023-04-24 13:35:17 +0200

    add id option

--- serializers.py
+++ serializers.py
@@ -9,14 +9,14 @@ class PlaceSerializer(DynamicDepthSerializer):
 
     class Meta:
         model = Place
-        fields = get_fields(Place, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Place, exclude=DEFAULT_FIELDS)+ ['id']
 
 
 class PlaceGeoSerializer(GeoFeatureModelSerializer):
 
     class Meta:
         model = Place
-        fields = get_fields(Place, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Place, exclude=DEFAULT_FIELDS)+ ['id']
         geo_field = 'geometry'
         depth = 1
 
@@ -25,18 +25,18 @@ class TIFFImageSerializer(DynamicDepthSerializer):
 
     class Meta:
         model = Image
-        fields = get_fields(Image, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Image, exclude=DEFAULT_FIELDS)+ ['id']
 
 
 class LayerSerializer(DynamicDepthSerializer):
 
     class Meta:
         model = Layer
-        fields = get_fields(Layer, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Layer, exclude=DEFAULT_FIELDS)+ ['id']
 
 
 class SourceSerializer(DynamicDepthSerializer):
 
     class Meta:
         model = Source
-        fields = get_fields(Source, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Source, exclude=DEFAULT_FIELDS)+ ['id']
