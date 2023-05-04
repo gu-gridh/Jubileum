c69141189487786088e9ab57f687e0e0365882b1
AramKarimi <aram.karimi@gu.se>
2023-05-04 10:49:44 +0200

    remove type cascade

--- models.py
+++ models.py
@@ -50,7 +50,7 @@ class Image(abstract.AbstractTIFFImageModel):
     title = models.CharField(max_length=1024, null=True, blank=True, verbose_name=_("title"))
     source_id = models.ForeignKey(Source, on_delete=models.CASCADE, null=True, blank=True)
     place   = models.ForeignKey(Place, null=True, blank=True, on_delete=models.CASCADE, related_name="images")
-    type = models.ForeignKey(ImageTypeTag, on_delete=models.CASCADE, null=True, blank=True, help_text=_("Type of the image can be jpeg, png, etc."))
+    type = models.CharField(max_length=32, null=True, blank=True, help_text=_("Type of the image can be jpeg, png, etc."))
     image_url = models.CharField(max_length=256, blank=True, null=True)
     description = models.TextField(null=True, blank=True, help_text=("Descriptive text about the the images"))
 
